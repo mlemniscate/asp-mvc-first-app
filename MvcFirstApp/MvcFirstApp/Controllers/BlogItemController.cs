66472f7a8e1ef87a75b1098f543b4a61e40d908b
4000665 <4000665@ANSARI-SHAHROUZ>
2022-01-20 10:28:42 +0330

    Add BlogItem and its controller. Make some changes in Save file.

---
+++ MvcFirstApp/MvcFirstApp/Controllers/BlogItemController.cs
@@ -0,0 +1,23 @@
+﻿using MvcFirstApp.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcFirstApp.Controllers
+{
+    public class BlogItemController : Controller
+    {
+        public ActionResult Save()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Save(BlogItem item)
+        {
+            return View();
+        }
+    }
+}
---
+++ MvcFirstApp/MvcFirstApp/Models/Blog/BlogItem.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MvcFirstApp.Models
+{
+    public class BlogItem
+    {
+        public string Titr { get; set; }
+        public string Description { get; set; }
+        public string Link { get; set; }
+    }
+}
